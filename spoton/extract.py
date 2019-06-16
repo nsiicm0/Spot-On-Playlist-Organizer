1fad590b059b82929ea9b0a0e9ad4d16c42808a3
Niclas Simmler <niclas.simmler@ch.pwc.com>
2019-06-16 22:20:06 +0200

    Extraction logic implemented
    Extraction logic implemented. All track information which is now extracted, will be pickled to disk.

--- spoton/extract.py
+++ spoton/extract.py
@@ -1,4 +1,9 @@
 import logging
+from .utils import Spoton_Util
+import spotipy
+import pandas as pd
+import itertools
+from datetime import datetime
 
 module_logger  = logging.getLogger('spoton.extract')
 
@@ -15,5 +20,67 @@ class Spoton_Extract():
 
     def __init__(self):
         self.logger = logging.getLogger('spoton.extract.Extractor')
+        self.config = Spoton_Util.load_config()
+        self.token = Spoton_Util.auth_spotify()
         self.logger.debug('Starting extraction')
-        self.logger.debug('Done with extraction')
+
+    def _get_track(self, results):
+        """ 
+        Helper function to get track ids from api results
+
+        Args:
+            none
+        
+        Returns:
+            Yields list of trackids
+        """
+        for item in results['items']:
+            track = item['track']
+            yield(track['id'])
+
+    def extract(self):
+        """ 
+        Track metadata from spotify api.
+        Results will be pickled and stored to disk.
+        
+        Args:
+            none
+        
+        Returns:
+            none
+        """
+        sp = spotipy.Spotify(auth=self.token)
+        sp.trace = False
+        
+        # Query playlistid
+        results = sp.current_user_playlists()
+        playlistid = [x['id'] for x in results['items'] if x['name'] == self.config['spotify']['playlist']][0]
+        self.logger.debug('Found playlist with the id {}'.format(playlistid))
+
+        # Query tracks from playlist
+        results = sp.user_playlist(self.config['spotify']['username'], playlistid, fields="tracks,next")
+        tracks = results['tracks']
+        track_ids = list(self._get_track(tracks))
+        while tracks['next']:
+            tracks = sp.next(tracks)
+            track_ids.extend(list(self._get_track(tracks)))
+        self.logger.debug('Retrieved {} tracks'.format(str(len(track_ids))))
+        
+        # Get track details
+        start = 0
+        end = 50
+        trackinfo = []
+        trackfeatures = []
+        # we have to loop in chunks of 50 due to api limitation
+        for i in range(0, len(track_ids), 50):
+            _info = sp.tracks(track_ids[i:i+50])
+            _features = sp.audio_features(track_ids[i:i+50])
+            trackinfo.extend(_info['tracks'])
+            trackfeatures.extend(_features)
+        self.logger.debug('Fetched all track metadata.')
+        pd_trackinfo = pd.DataFrame(trackinfo)
+        pd_trackinfo.to_pickle('{}/trackinfo_{}.pkl'.format(self.config['app']['data']['path'],datetime.now().strftime('%Y%m%d%H%M'))) 
+        pd_trackfeatures = pd.DataFrame(trackfeatures)
+        pd_trackfeatures.to_pickle('{}/trackfeatures_{}.pkl'.format(self.config['app']['data']['path'],datetime.now().strftime('%Y%m%d%H%M')))
+        self.logger.debug('Pickled all track metadata.') 
+        self.logger.debug('Done extracting.') 
